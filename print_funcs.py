731b31736fe017df9bec7ce5cc13d0c69f11fa4f
RomacH <RomacHGaminG@gmail.com>
2022-07-15 19:06:29 +0300

    added type hints

--- print_funcs.py
+++ print_funcs.py
@@ -1,17 +1,26 @@
 from pyfiglet import Figlet
 from termcolor import cprint
 
-# ? Functions for colorful logging
+# Functions for colorful logging
 
 
-def error(prefix: str, ex):
+def error(prefix: str, ex: Exception) -> None:
     cprint(f"[{prefix.upper()}_ERROR] {repr(ex)}", "red")
 
 
-def info(text: str):
+def info(text: str) -> None:
     cprint(f"[INFO] {text}", "green")
 
 
-def logo(color: str):
+def special_info(text: str) -> None:
+    cprint(f"[INFO] {text}", "yellow")
+
+
+def progress(counter: int, max_pos: int) -> None:
+    cprint(f"[+] Обработано {counter}/{max_pos} страниц", "cyan")
+
+
+def logo(color: str) -> None:
     preview = Figlet(font="doom")
-    cprint(preview.renderText("RomacH"), color)
+    logo_str = f"{preview.renderText('RomacH').strip()}\n"
+    cprint(logo_str, color)
